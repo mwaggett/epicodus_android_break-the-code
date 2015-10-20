195f5e7f9420a0982d18c284d6cda602500bebb1
Molly Waggett <mwaggett@alumni.reed.edu>
2015-10-20 10:02:32 -0700

    Set onClick listeners on buttons to navigate to appropriate pages.

---
+++ app/src/main/java/com/epicodus/breakthecode/NonProfitActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.breakthecode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NonProfitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_non_profit);
+    }
+}
